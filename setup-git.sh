860e4a2b12d01064932768efdfe7717249a6e5fd
Erik Dubois <erik.dubois@gmail.com>
2016-08-05 08:15:21 +0200

    Automatic Backup @ Fri Aug  5 08:15:21 CEST 2016

---
+++ setup-git.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+#======================================================================================
+#
+#                  -`
+#                  .o+`                 
+#                 `ooo/                  
+#                `+oooo:                 M  a  d  e    on 
+#               `+oooooo:               
+#               -+oooooo+:              
+#             `/:-:++oooo+:             
+#            `/++++/+++++++:                      A  R  C  H
+#           `/++++++++++++++:           
+#          `/+++ooooooooooooo/`         
+#         ./ooosssso++osssssso+`        
+#        .oossssso-````/ossssss+`       
+#       -osssssso.      :ssssssso.          F  o   r   
+#      :osssssss/        osssso+++.     
+#     /ossssssss/        +ssssooo/-     
+#   `/ossssso+/:-        -:/+osssso+-                           A   R   C   H
+#  `+sso+:-`                 `.-/+oso: 
+# `++:.                           `-/+/
+# .`                                 `/
+#
+#======================================================================================
+#
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+# 
+# More from Erik Dubois
+#
+# Aurora Conky
+# at http://sourceforge.net/projects/auroraconkytheme/
+# Explanation on the use of this theme can be found at 
+# http://erikdubois.be/category/linux/aurora-conky/
+# 
+# Aureola Conky
+# Collections of nice conky's with lua syntax
+# https://github.com/erikdubois/Aureola
+#
+# Sardi icons
+# Many different styles of icons from colourfull, monochrome, white, circle
+# https://sourceforge.net/projects/sardi/
+#
+# Super Ultra Flat Numix Remix
+# Colourfull and playfull icons
+# https://github.com/erikdubois/Super-Ultra-Flat-Numix-Remix
+#
+# Check out the github - many more scripts for automatic installation of Linux Systems.
+#
+#
+#
+#
+#
+##################################################################################################################
+# If the option -y has been added. It will autoinstall all. Omit if you do not want that.
+##################################################################################################################
+#
+#
+#
+#
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+
+#checking if git is installed else install it
+
+if ! location="$(type -p "git")" || [ -z "git" ]; then
+
+	echo "#################################################"
+	echo "installing git for this script to work"
+	echo "#################################################"
+
+  	sudo apt install git -y
+fi
+
+
+
+
+#setting up git
+#https://www.atlassian.com/git/tutorials/setting-up-a-repository/git-config
+
+git init
+git config --global user.name "Erik Dubois"
+git config --global user.email "erik.dubois@gmail.com"
+sudo git config --system core.editor nano
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
+git config --global push.default simple
+
+
+
+#git remote add origin https://github.com/erikdubois/ArchCinnamon
+
+
+echo " ALL  D O N E !"
