dc87c4f6c8a751e467227a0be719a84a92aff928
Erik Dubois <erik.dubois@gmail.com>
2017-01-31 16:25:17 +0100

    Comment :  on 16:25:17 31-01-2017

--- installation/050-install-cinnamon-core-v1.sh
+++ installation/050-install-cinnamon-core-v1.sh
@@ -12,8 +12,6 @@ set -e
 ##################################################################################################################
 
 
-# gnome
-
 echo "################################################################"
 echo "cinnamon"   
 echo "################################################################"
@@ -53,7 +51,7 @@ else
 fi
 
 
-package="gdm"
+package="mdm-display-manager"
 
 #----------------------------------------------------------------------------------
 
@@ -87,60 +85,6 @@ else
 
 fi
 
-
-
-
-echo "################################################################"
-echo "budgie-desktop"   
-echo "################################################################"
-
-package="budgie-desktop"
-
-#----------------------------------------------------------------------------------
-
-#checking if application is already installed or else install with aur helpers
-if pacman -Qi $package &> /dev/null; then
-
-	echo "################################################################"
-	echo "################## "$package" is already installed"
-	echo "################################################################"
-
-else
-
-	#checking which helper is installed
-	if pacman -Qi packer &> /dev/null; then
-
-		echo "Installing with packer"
-		packer -S --noconfirm --noedit  $package
-
-	elif pacman -Qi pacaur &> /dev/null; then
-		
-		echo "Installing with pacaur"
-		pacaur -S --noconfirm --noedit  $package
-		 	
-	elif pacman -Qi yaourt &> /dev/null; then
-
-		echo "Installing with yaourt"
-		yaourt -S --noconfirm $package
-			  	
-	fi
-
-	# Just checking if installation was successful
-	if pacman -Qi $package &> /dev/null; then
-	
-	echo "################################################################"
-	echo "#########  "$package" has been installed"
-	echo "################################################################"
-
-	else
-
-	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-	echo "!!!!!!!!!  "$package" has NOT been installed"
-	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-
-	fi
-
-fi
 
 
 
@@ -149,7 +93,7 @@ fi
 
 echo "Display manager being activated"
 
-sudo systemctl enable gdm.service
+sudo systemctl enable mdm.service
 
 echo "Reboot and select the proper desktop environment"
 echo "with the gauge symbol."
